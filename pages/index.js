eec872106449b4a81b8a1fabf3cc5add9e849dd4
juhek2525 <junhek2525@gmail.com>
2024-09-20 14:09:51 +0900

    9_20

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,46 @@
-import { useEffect } from "react";
+import { Container,Table,TableBody, TableHead ,TableRow,TableCell } from "@mui/material"
+import Link from "next/link";
+import Button from "@mui/material/Button";
+
+
+function createData(name,github,link){
+    return {name,github,link};
+}
+const rows = [
+  createData('duduge','https://github.com/junhek2525/pr-demo','/duduge'),
+  createData('momery','https://github.com/junhek2525/pr-demo','/momery'),
+  createData('snake','https://github.com/junhek2525/pr-demo','/snake')
+]
+
 
 export default function Home() {
 
   return(
     <>
-    
+      <Container fixed>
+    <h1>니하오</h1>
+    <Table sx={{minWidth : 650}} aria-Label="demo-list">
+      <TableHead>
+        <TableRow>
+          <TableCell>Demo-List</TableCell>
+          <TableCell>Github</TableCell>
+          <TableCell>Link</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row) =>(
+          <TableRow
+            key={row.name}
+            sx={{'&:last-child td, &:last-child th': {border:0}}}
+          >
+            <TableCell component="th" scope="row">{row.name}</TableCell>
+            <TableCell><Link href={row.github}>깃허브 바로가기</Link></TableCell>
+            <TableCell><Button variant="outlined" size="small" href={row.link}>Link</Button></TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+      </Container>
     </>
   )
   // useEffect (() =>{
